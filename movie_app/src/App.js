4ab9eb9e5af952091aadd4d6a5a9e3769f733012
Choieeh <chyh0182@naver.com>
2021-01-25 23:21:28 +0000

    Component basic

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
+function Food({name, number}){
+	return <div>
+		<h1>I like {name}</h1>
+		<h3>{number}</h3>
+	</div>
+}
+
+const foodILike = [
+	{
+		name : "kimchi",
+		number : "1"
+	},
+	{
+		name : "sampgeaop",
+		number : "2"
+	},
+	{
+		name : "kimbap",
+		number : "3"
+	}
+]
+
 function App() {
   return (
     <div className="App">
-      <h1>asdf</h1>Hello!!!!
+    	<h1>asdf</h1>Hello!!!!
+		{foodILike.map(dish =>(
+			  <Food name={dish.name} number={dish.number}/>
+			  //dish는 object를 받아온것.
+		))}
     </div>
   );
 }
