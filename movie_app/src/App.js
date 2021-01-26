d61c99258faf7a1a9eeb314fccbb846986249fa0
Choieeh <chyh0182@naver.com>
2021-01-26 13:48:29 +0000

    class component

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,53 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Food({name, number, rating}){
-	return <div>
-		<h3>{number}</h3>
-		<h1>I like {name}</h1>
-		<h4>{rating} / 5</h4>
-	</div>
-}
-
-const foodILike = [
-	{
-		id:1,
-		name : "kimchi",
-		number : "1",
-		rating : 5
-	},
-	{
-		id:2,
-		name : "sampgeaop",
-		number : "2",
-		rating : 4.9
-	},
-	{
-		id:3,
-		name : "kimbap",
-		number : "3",
-		rating : 3.8
+class App extends React.Component{
+	state = {
+		count : 0
+	};
+	add = () => {};
+	minus = () => {};
+	render(){
+		return 
+			<div>
+			<h1>The number is : {this.state.count}</h1>
+			<button onClick={this.add}>Add</button>
+			<button onClick={this.minus}>Minus</button>
+			</div>
 	}
-]
-
-Food.propTypes = {
-	name: PropTypes.string.isRequired,
-	number : PropTypes.string.isRequired,
-	rating : PropTypes.number.isRequired
-}
-
-function renderFood(dish){
-	console.log(dish);
-	return <Food key={dish.id} name = {dish.name} number = {dish.number} rating = {dish.rating}/>
-}
-
-function App() {
-  return (
-    <div className="App">
-    	<h1>asdf</h1>Hello!!!!
-		{foodILike.map(renderFood)}
-    </div>
-  );
 }
 
 export default App;
